3830552d4f8030d4438f9ea7c2983f082ee8d935
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2021-06-26 18:22:10 +0200

    Setup build process to bundle.

---
+++ packages/slide-show/rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import copy from 'rollup-plugin-copy';
+import versionInjector from 'rollup-plugin-version-injector';
+
+import * as packageInfo from './package.json';
+
+// Static assets will vary depending on the application
+const copyConfig = {
+  targets: [
+  ],
+};
+
+const plugins = [
+  minifyHTML(),
+  copy(copyConfig),
+  resolve(),
+  versionInjector(),
+]
+
+const minPlugins = [
+  minifyHTML(),
+  copy(copyConfig),
+  resolve(),
+  terser(),
+  versionInjector(),
+]
+
+// The main JavaScript bundle for modern browsers that support
+// JavaScript modules and other ES2015+ features.
+const config = (plugins, outName) => ({
+  input: 'slide-show.js',
+  output: {
+    file: `dist/${outName}`,
+    format: 'es',
+  },
+  plugins: plugins,
+  preserveEntrySignatures: false,
+});
+
+// if (process.env.NODE_ENV !== 'development') {
+//   config.plugins.push(terser());
+// }
+
+export default [
+  config(plugins, `slide-show_${packageInfo.version}.js`),
+  config(minPlugins, `slide-show_${packageInfo.version}.min.js`)
+];
