fdddd50f9a4a18c125adab61507e5db09fc3c247
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2021-06-26 15:06:09 +0200

    Add slide-show buttons customization

--- packages/slide-show/src/SlideShow.js
+++ packages/slide-show/src/SlideShow.js
@@ -48,27 +48,105 @@ export class SlideShow extends LitElement {
 
       :host slide-show-controls {
         position: absolute;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        height: 40px;
-
-        display: flex;
-        flex-direction: row;
-        flex-wrap: nowrap;
-        justify-content: space-between;
-      }
-
-      :host slide-show-controls > * {
-        height: 100%;
+        left: 0.5em;
+        bottom: 0.5em;
+        top: 0.5em;
+        width: calc(100% - 1em);
       }
 
       :host slide-show-controls > slide-selectors {
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
-
         align-items: center;
+        /* height: 100%; */
+
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        transform: translateX(-50%);
+      }
+
+      :host slide-show-controls button {
+        border: none;
+        cursor: pointer;
+        padding: 0;
+      }
+
+      :host slide-show-controls > button {
+        position: absolute;
+        border-radius: 50%;
+        height: 3em;
+        width: 3em;
+        bottom: 50%;
+        transform: translateY(50%);
+        background-color: var(--slide-show-nav-button-bg);
+      }
+
+      :host slide-show-controls > button#previous-slide {
+        left: 0;
+      }
+
+      :host slide-show-controls > button#next-slide {
+        right: 0;
+      }
+
+      :host slide-show-controls > slide-selectors > button {
+        background-color: var(--slide-show-select-button-bg);
+      }
+
+      :host slide-show-controls > slide-selectors > button:nth-child(n+2) {
+        margin-left: 0.5em;
+      }
+
+      :host slide-show-controls > slide-selectors > button.active {
+        background-color: var(--slide-show-active-button-bg);
+      }
+
+      :host .icon-previous,
+      :host .icon-next {
+        position: absolute;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 100%;
+      }
+
+      :host .icon-previous::before,
+      :host .icon-next::before {
+        content: "";
+
+        position: absolute;
+        left: 20%;
+        top: 20%;
+        right: 20%;
+        bottom: 20%;
+
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+
+      :host .icon-previous::before {
+        background-image: var(--slide-show-previous-icon);
+      }
+
+      :host .icon-next::before {
+        background-image: var(--slide-show-next-icon);
+      }
+
+      :host .icon-selector {
+        display: block;
+        width: 1.5em;
+        height: 0.3em;
+      }
+
+      :host .hidden-text {
+        display: none;
+      }
+
+      :host .hidden-text:before {
+        display: none;
       }
     `;
   }
@@ -93,7 +171,7 @@ export class SlideShow extends LitElement {
 
   constructor() {
     super();
-    
+
     this.activeIndex = -1;
     this.intervalTime = -1;
     this.images = [];
@@ -212,16 +290,26 @@ export class SlideShow extends LitElement {
   _controlsTemplate(images) {
     return html`
       <slide-show-controls>
-        <button id="previous-slide" @click=${this._previousIndex}>Prev</button>
+        <button id="previous-slide" @click=${this._previousIndex}>
+          <span class="icon-previous" aria-hidden="true"></span>
+          <span class="hidden-text">Previous Slide</span>
+        </button>
         <slide-selectors>
           ${images.map(image => {
             const index = image.getAttribute('index') ?? 0;
+            const active = index == this.activeIndex ? 'active' : '';
             return html`
-              <button @click=${this._setIndex} index="${index}">${index}</button>
+              <button index="${index}" class="${active}" @click=${this._setIndex}>
+                <span class="icon-selector" aria-hidden="true"></span>
+                <span class="hidden-text">Show slide ${index+1}</span>
+              </button>
             `
           })}
         </slide-selectors>
-        <button id="next-slide" @click=${this._nextIndex}>Next</button>
+        <button id="next-slide" @click=${this._nextIndex}>
+          <span class="icon-next" aria-hidden="true"></span>
+          <span class="hidden-text">Next Slide</span>
+        </button>
       </slide-show-controls>
     `;
   }
